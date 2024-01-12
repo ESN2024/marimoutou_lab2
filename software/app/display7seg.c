cf2b00a37402947b2f40846cebe6a99fc726e788
Mourougen MRMT <97475510+MourouMRMT@users.noreply.github.com>
2024-01-12 16:28:51 +0100

    ajout 2 autres segments

--- software/app/display7seg.c
+++ software/app/display7seg.c
@@ -10,19 +10,33 @@
 int main()
 {
 	alt_printf("main() started\n");
-	
-	uint8_t data=0;
-	
+	int i=0;
+	int data=0;
+	uint8_t data_tab[3]={0,0,0};
 	
 	while(1)
-	{
-		IOWR_ALTERA_AVALON_PIO_DATA(SEG1_BASE,data);
-		data++;
-		usleep(200000);
-		if(data>9)
-		{
-			data=0;
-		}
+	{	
+		if(data<999)
+		{	
+			if(data>10)
+			{	
+				data_tab[i] = data % 10;
+				data = data / 10;
+				i = i+1;
+			}
+			data_tab[i]=data;
+			
+			IOWR_ALTERA_AVALON_PIO_DATA(SEG1_BASE,data_tab[0]);
+			IOWR_ALTERA_AVALON_PIO_DATA(SEG2_BASE,data_tab[1]);
+			IOWR_ALTERA_AVALON_PIO_DATA(SEG3_BASE,data_tab[2]);
+			
+			data++;
+			alt_printf("%d",data);
+			usleep(200000);
+			
+		}else{data=0;}
+		
+
 	}
 	return 0;
 }
--- toplevel.vhd
+++ toplevel.vhd
@@ -6,7 +6,9 @@ entity toplevel is
 	port (
 		CLK      : in  std_logic                    := '0'; --   clk.clk
 		RESET : in  std_logic                    := '0'; -- reset.reset_n
-		SEG1   : out std_logic_vector(7 downto 0)         --  seg1.export
+		SEG1   : out std_logic_vector(7 downto 0);         --  seg1.export
+		SEG2   : out std_logic_vector(7 downto 0);        --  seg2.export
+		SEG3   : out std_logic_vector(7 downto 0)         --  seg3.export
 	);
 end entity toplevel;
 
@@ -16,7 +18,9 @@ architecture rtl of toplevel is
 		port (
 			clk_clk       : in  std_logic                    := 'X'; -- clk
 			reset_reset_n : in  std_logic                    := 'X'; -- reset_n
-			seg1_export   : out std_logic_vector(3 downto 0)         -- export
+			seg1_export   : out std_logic_vector(3 downto 0);         -- export
+			seg2_export   : out std_logic_vector(3 downto 0);       -- export
+			seg3_export   : out std_logic_vector(3 downto 0)         -- export
 		);
 	end component lab2;
 	
@@ -28,14 +32,17 @@ architecture rtl of toplevel is
 	end component BCD7SEG;
 	
 signal segment1 : std_logic_vector(3 downto 0);
-
+signal segment2 : std_logic_vector(3 downto 0);
+signal segment3 : std_logic_vector(3 downto 0);
 begin
 
 	u0 : component lab2
 		port map (
 			clk_clk       => CLK,       --   clk.clk
 			reset_reset_n => RESET, -- reset.reset_n
-			seg1_export   => segment1    --  seg1.export
+			seg1_export   => segment1,    --  seg1.export
+			seg2_export   => segment2,   --  seg2.export
+			seg3_export   => segment3    --  seg3.export
 		);
 		
 	u1 : component BCD7SEG
@@ -44,4 +51,15 @@ begin
 			output => SEG1
 		);
 
+	u2 : component BCD7SEG
+		port map (
+			input => segment2,
+			output => SEG2
+		);
+		
+	u3 : component BCD7SEG
+		port map (
+			input => segment3,
+			output => SEG3
+		);
 end architecture;
